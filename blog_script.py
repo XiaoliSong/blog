18a971ba979526bee0c5ab567b0404b1d7dc861e
XiaoliSong <321219693@qq.com>
2018-09-04 19:03:42 +0800

    add：脚本添加普通页面

--- blog_script.py
+++ blog_script.py
@@ -2,30 +2,38 @@
 import os
 import sys
 
-
 SCRIPT_PREFIX = 'python ./python/'
 
 
 def exec_recomend():
     os.system(SCRIPT_PREFIX + 'special_pages/recommend.py')
 
+
 def exec_archive():
     os.system(SCRIPT_PREFIX + 'special_pages/archive.py')
+
 
 def exec_tag():
     os.system(SCRIPT_PREFIX + 'special_pages/tag.py')
 
+
 def exec_page():
     os.system(SCRIPT_PREFIX + 'page.py')
 
+
+def exec_general_pages():
+    os.system(SCRIPT_PREFIX + 'general_pages.py')
+
+
 def exec_rss():
     os.system(SCRIPT_PREFIX + 'rss.py')
+
 
 def exec_sitemap():
     os.system(SCRIPT_PREFIX + 'sitemap.py')
 
 
-def exec_al_posts():
+def exec_all_posts():
     os.system(SCRIPT_PREFIX + 'posts.py')
     os.system(SCRIPT_PREFIX + 'post_json.py')
     exec_archive()
@@ -37,10 +45,12 @@ def exec_al_posts():
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        if sys.argv[1]=='recommend':
+        if sys.argv[1] == 'recommend':
             exec_recomend()
-        elif sys.argv[1]=='post':
-            exec_al_posts()
+        elif sys.argv[1] == 'post':
+            exec_all_posts()
+        elif sys.argv[1] == 'general':
+            exec_general_pages()
         else:
             print('参数错误')
     else:
